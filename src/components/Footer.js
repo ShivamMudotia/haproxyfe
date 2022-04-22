61d9afa98cfd1a3549c6a73fc0ecf12f91a4a777
Shivam <shivammudotia@IN-FVFF8FJZQ05D.local>
2022-04-22 14:21:19 +0530

    updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer text-center bg-dark text-warning fw-bold">
-    <p>Created by @Shivam Mudotia  &nbsp; &nbsp; | &nbsp; &nbsp;  @Free to use and disrtibute &nbsp; &nbsp; | &nbsp; &nbsp;  @MIT License &nbsp; &nbsp;  | &nbsp; &nbsp; Source Code at  -> https://github.com/ShivamMudotia/haproxyfe</p>
+    <p>Created by @Shivam Mudotia  &nbsp; &nbsp; | &nbsp; &nbsp;  @Free to use and disrtibute &nbsp; &nbsp; | &nbsp; &nbsp;  @MIT License &nbsp; &nbsp;  | &nbsp; &nbsp; @Source Code at  -> https://github.com/ShivamMudotia/haproxyfe</p>
   </div>
 );
 
